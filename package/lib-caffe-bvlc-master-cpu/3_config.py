e6d876820a16510607dab20435b9d60fa4dd2f25
Anton Lokhmotov <anton@dividiti.com>
2016-06-16 17:52:45 +0000

    Update package:lib-caffe-bvlc-master-cpu.

--- package/lib-caffe-bvlc-master-cpu/3_config.py
+++ package/lib-caffe-bvlc-master-cpu/3_config.py
@@ -9,14 +9,20 @@ def update(settings):
   ck_cxx = os.environ.get('CK_CXX')
   if ck_cxx:
     settings['custom_cxx'] = ck_cxx
+  # BLAS library dependency.
+  open_blas = os.environ.get('CK_ENV_LIB_OPENBLAS')
+  if open_blas:
+    settings['cpu_blas'] = 'open'
+    settings['cpu_blas_lib'] = os.path.join(open_blas, 'lib') 
+    settings['cpu_blas_include'] = os.path.join(open_blas, 'include')
   return settings
 
 def generate(template, settings):
   config = template.format(
     cpu_only         = settings['cpu_only'        ],
     cpu_blas         = settings['cpu_blas'        ],
-    cpu_blas_include = settings['cpu_blas_include'],
     cpu_blas_lib     = settings['cpu_blas_lib'    ],
+    cpu_blas_include = settings['cpu_blas_include'],
     use_opencv       = settings['use_opencv'      ],
     opencv_version   = settings['opencv_version'  ],
     use_leveldb      = settings['use_leveldb'     ],
