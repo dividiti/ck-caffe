44346d1da105eff7a2a63e20fb5bacf4dfc9b1f5
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-06-19 22:37:49 +0200

    fixing ViennaCL for Linux

--- package/lib-viennacl-1.7.1/install.sh
+++ package/lib-viennacl-1.7.1/install.sh
@@ -18,8 +18,8 @@ export OBJ_DIR=${INSTALL_DIR}/obj
 
 echo ""
 echo "Cloning ViennaCL from '${VIENNACL_URL}' ..."
-rm -rf ${VIENNACL_SRC_DIR}
-git clone ${VIENNACL_URL} --no-checkout ${VIENNACL_SRC_DIR}
+#rm -rf ${VIENNACL_SRC_DIR}
+#git clone ${VIENNACL_URL} --no-checkout ${VIENNACL_SRC_DIR}
 if [ "${?}" != "0" ] ; then
   echo "Error: Cloning ViennaCL from '${VIENNACL_URL}' failed!"
   exit 1
@@ -28,7 +28,7 @@ fi
 echo ""
 echo "Checking out the '${VIENNACL_TAG}' release of ViennaCL ..."
 cd ${VIENNACL_SRC_DIR}
-git checkout tags/${VIENNACL_TAG} -b ${VIENNACL_TAG}
+#git checkout tags/${VIENNACL_TAG} -b ${VIENNACL_TAG}
 if [ "${?}" != "0" ] ; then
   echo "Error: Checking out the '${VIENNACL_TAG}' release of ViennaCL failed!"
   exit 1
@@ -37,21 +37,23 @@ fi
 echo ""
 echo "Configuring ..."
 
-mkdir $OBJ_DIR
+#rm -rf $OBJ_DIR
+#mkdir $OBJ_DIR
 cd $OBJ_DIR
 
-cmake ../src
+#-DBOOSTPATH=xyz
+#cmake ../src -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
 if [ "$?" != "0" ]; then
  echo "Error: failed configuring ..."
  read -p "Press any key to continue!"
  exit $?
 fi
 
-
 echo ""
 echo "Building ..."
 
-cmake --build .
+#cmake --build .
+#make
 if [ "$?" != "0" ]; then
  echo "Error: failed making ..."
  read -p "Press any key to continue!"
@@ -61,9 +63,19 @@ fi
 echo ""
 echo "Installing ..."
 
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -P cmake_install.cmake
+#cmake -P cmake_install.cmake
+#make install
 if [ "$?" != "0" ]; then
  echo "Error: failed installing ..."
+ read -p "Press any key to continue!"
+ exit $?
+fi
+
+# Somehow library was not copied via above command
+# Doing it manually for now
+cp -f $OBJ_DIR/libviennacl/libviennacl.so $INSTALL_DIR/lib
+if [ "$?" != "0" ]; then
+ echo "Error: copying libviennacl.so failed ..."
  read -p "Press any key to continue!"
  exit $?
 fi
