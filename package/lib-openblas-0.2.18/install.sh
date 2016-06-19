0d3a929313a9fa80d7b862def676938b84587faa
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-06-19 18:41:48 +0200

    fixes (not yet finished)

--- package/lib-openblas-0.2.18/install.sh
+++ package/lib-openblas-0.2.18/install.sh
@@ -18,8 +18,8 @@ export OPENBLAS_BLD_DIR=${OPENBLAS_SRC_DIR}
 
 echo ""
 echo "Cloning OpenBLAS from '${OPENBLAS_URL}' ..."
-rm -rf ${OPENBLAS_SRC_DIR}
-git clone ${OPENBLAS_URL} --no-checkout ${OPENBLAS_SRC_DIR}
+#rm -rf ${OPENBLAS_SRC_DIR}
+#git clone ${OPENBLAS_URL} --no-checkout ${OPENBLAS_SRC_DIR}
 if [ "${?}" != "0" ] ; then
   echo "Error: Cloning OpenBLAS from '${OPENBLAS_URL}' failed!"
   exit 1
@@ -28,7 +28,7 @@ fi
 echo ""
 echo "Checking out the '${OPENBLAS_TAG}' release of OpenBLAS ..."
 cd ${OPENBLAS_SRC_DIR}
-git checkout tags/${OPENBLAS_TAG} -b ${OPENBLAS_TAG}
+#git checkout tags/${OPENBLAS_TAG} -b ${OPENBLAS_TAG}
 if [ "${?}" != "0" ] ; then
   echo "Error: Checking out the '${OPENBLAS_TAG}' release of OpenBLAS failed!"
   exit 1
@@ -38,8 +38,22 @@ echo ""
 echo "Building the '${OPENBLAS_TAG}' release of OpenBLAS ..."
 mkdir -p ${OPENBLAS_BLD_DIR}
 cd ${OPENBLAS_BLD_DIR}
-# TODO: get TARGET from ENV?
-make libs -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} TARGET=ARMV7
+
+# Check if ARM target
+TARGET=""
+if [ "${CK_ANDROID_NDK_ARCH}" == "arm" ] ; then
+   export CC=${CK_CC}
+   export FC=${CK_FC}
+   export AR=${CK_AR}
+   TARGET="OSNAME=Android TARGET=ARMV7"
+elif [ "${CK_ANDROID_NDK_ARCH}" == "arm64" ] ; then
+   export CC=${CK_CC}
+   export FC=${CK_FC}
+   export AR=${CK_AR}
+   TARGET="OSNAME=Android TARGET=ARMV8"
+fi
+
+#make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} $TARGET
 if [ "${?}" != "0" ] ; then
   echo "Error: Building the '${OPENBLAS_TAG}' release of OpenBLAS failed!"
   exit 1
--- package/lib-viennacl-1.7.1/install.sh
+++ package/lib-viennacl-1.7.1/install.sh
@@ -14,6 +14,7 @@
 # INSTALL_DIR
 
 export VIENNACL_SRC_DIR=${INSTALL_DIR}/src
+export OBJ_DIR=${INSTALL_DIR}/obj
 
 echo ""
 echo "Cloning ViennaCL from '${VIENNACL_URL}' ..."
@@ -31,4 +32,38 @@ git checkout tags/${VIENNACL_TAG} -b ${VIENNACL_TAG}
 if [ "${?}" != "0" ] ; then
   echo "Error: Checking out the '${VIENNACL_TAG}' release of ViennaCL failed!"
   exit 1
+fi
+
+echo ""
+echo "Configuring ..."
+
+mkdir $OBJ_DIR
+cd $OBJ_DIR
+
+cmake ../src
+if [ "$?" != "0" ]; then
+ echo "Error: failed configuring ..."
+ read -p "Press any key to continue!"
+ exit $?
+fi
+
+
+echo ""
+echo "Building ..."
+
+cmake --build .
+if [ "$?" != "0" ]; then
+ echo "Error: failed making ..."
+ read -p "Press any key to continue!"
+ exit $?
+fi
+
+echo ""
+echo "Installing ..."
+
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -P cmake_install.cmake
+if [ "$?" != "0" ]; then
+ echo "Error: failed installing ..."
+ read -p "Press any key to continue!"
+ exit $?
 fi
