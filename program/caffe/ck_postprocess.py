c8ba6daab4bade5908afb87505dafa6f7b7f19c3
Anton Lokhmotov <anton@dividiti.com>
2016-06-24 16:39:06 +0000

    Postprocess layer info, fw, bw, fw+bf execution times.

--- program/caffe/ck_postprocess.py
+++ program/caffe/ck_postprocess.py
@@ -6,44 +6,91 @@
 
 import json
 import os
+import re
 
-d={}
 
 def ck_postprocess(i):
-
     ck=i['ck_kernel']
 
     d={}
 
-    # Load output as list
+    # Load output as list.
+    # NB: This assumes that Caffe iterates only once (--iterations=1).
+    # Otherwise, looping over the log would be required.
     r=ck.load_text_file({'text_file':'stderr.log','split_to_list':'yes'})
     if r['return']>0: return r
 
-    lst=r['lst']
-    for l in lst:
-        j=l.find('Total Time:')
-        if j>0:
-           l=l[j+12:]
+    d['per_layer_info']=[]
+    layer_index = 0
 
-           j1=l.find(' ')
-           if j1>0:
-              l=l[:j1]
+    for line in r['lst']:
+        # Match layer info.
+        layer_regex = 'caffe\.cpp:\d{3,4}](\s+)' + \
+            '(?P<label>[\w/_]+)(\s+)'  + \
+            '(?P<dir>forward|backward)(:\s+)' + \
+            '(?P<ms>\d*\.*\d*(e\+\d+)*) ms\.'
+        match = re.search(layer_regex, line)
+        if match:
+            info = {}
+            info['index'] = layer_index
+            info['label'] = '%s: %s' % (str(layer_index).zfill(2), match.group('label'))
+            info['direction'] = match.group('dir')
+            info['time_ms'] = float(match.group('ms'))
+            info['time_s'] = info['time_ms']*1e-3
+            d['per_layer_info'].append(info)
+            if info['direction'] == 'backward':
+                layer_index += 1
 
-              ttp=float(l)*1.0E-3
+        # Match forward execution time.
+        fw_regex = 'caffe\.cpp:\d{3,4}](\s+)' + \
+            'Average Forward pass:(\s+)' + \
+            '(?P<ms>\d*\.*\d*(e\+\d+)*) ms\.'
+        match = re.search(fw_regex, line)
+        if match:
+              d['time_fw_ms'] = float(match.group('ms'))
+              d['time_fw_s']= d['time_fw_ms']*1e-3
 
+        # Match backward execution time.
+        bw_regex = 'caffe\.cpp:\d{3,4}](\s+)' + \
+            'Average Backward pass:(\s+)' + \
+            '(?P<ms>\d*\.*\d*(e\+\d+)*) ms\.'
+        match = re.search(bw_regex, line)
+        if match:
+              d['time_bw_ms'] = float(match.group('ms'))
+              d['time_bw_s']= d['time_bw_ms']*1e-3
+
+        # Match forward-backward execution time.
+        fwbw_regex = 'caffe\.cpp:\d{3,4}](\s+)' + \
+            'Average Forward-Backward:(\s+)' + \
+            '(?P<ms>\d*\.*\d*(e\+\d+)*) ms\.'
+        match = re.search(fwbw_regex, line)
+        if match:
+              d['time_fwbw_ms'] = float(match.group('ms'))
+              d['time_fwbw_s']= d['time_fwbw_ms']*1e-3
+
+        # Match total execution time.
+        total_regex = 'caffe\.cpp:\d{3,4}](\s+)' + \
+            'Total Time:(\s+)' + \
+            '(?P<ms>\d*\.*\d*(e\+\d+)*) ms\.'
+        match = re.search(total_regex, line)
+        if match:
+              ms=float(match.group('ms'))
+              d['time_total_ms']=ms
+              d['time_total_ms_kernel_0']=ms
+              s=ms*1e-3
+              d['time_total_s']=s
+              d['time_total_s_kernel_0']=s
               d['post_processed']='yes'
-              d['execution_time']=ttp
-              d['execution_time_kernel_0']=ttp
-
-    rr={'return':0}
-
+    
+    rr={}
+    rr['return']=0
     if d.get('post_processed','')=='yes':
-       # Record to CK timer file
+       # Save to file.
        r=ck.save_json_to_file({'json_file':'tmp-ck-timer.json', 'dict':d})
        if r['return']>0: return r
     else:
        rr['return']=1
-       rr['error']='didn\'t manage to find Total Time string in Caffe output ...'
+       rr['error']='failed to find the \'Total Time\' string in Caffe output'
 
     return rr
 
