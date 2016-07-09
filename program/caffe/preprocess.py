a1d91a7d95e80d719d98028876da6fc7bd89748d
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-07-09 19:54:44 +0100

    adding support for auto-calculation of iterations ...

--- program/caffe/preprocess.py
+++ program/caffe/preprocess.py
@@ -14,14 +14,26 @@ def ck_preprocess(i):
     rt=i['run_time']
     deps=i['deps']
 
+    env=i['env']
+
     params=rt['params']
     cm_key=params['caffemodel_key']
+
+    b='\n'
+
+    # Get number of images
+    nim=deps.get('dataset-imagenet-lmdb',{}).get('dict',{}). \
+        get('customize',{}).get('features',{}).get('number_of_original_images','')
+    if nim=='':
+       return {'return':1, 'error':'can\'t find number of images in a dataset'}
+    nim=int(nim)
 
     # Find template
     x=deps['caffemodel']
     cm_path=x['dict']['env']['CK_ENV_MODEL_CAFFE']
 
     cus=x['cus']['params'][cm_key]
+
     template=cus['template']
 
     # load network topology template
@@ -42,6 +54,23 @@ def ck_preprocess(i):
 
     # Customize template
     sub=cus.get('substitute',{})
+
+    # Check batch_size
+    bs=sub.get('batch_size','')
+    if bs=='':
+       bs=sub.get('val_batch_size','')
+
+    iters=env.get('CK_CAFFE_ITERATIONS','')
+
+    if iters=='':
+       if bs=='':
+          iters=1
+       else:
+          iters=nim/bs
+
+       env['CK_CAFFE_ITERATIONS']=iters
+       b+='export CK_CAFFE_ITERATIONS='+str(iters)+'\n'
+
     for k in sub:
         v=sub[k]
         st=st.replace('$#'+k+'#$',str(v))
@@ -61,7 +90,8 @@ def ck_preprocess(i):
     if r['return']>0: return r
 
     # Prepare path to model
-    b='export CK_CAFFE_MODEL='+fn+'\n'
+    b+='export CK_CAFFE_MODEL='+fn+'\n'
+    env['CK_CAFFE_MODEL']=fn
 
     return {'return':0, 'bat':b}
 
