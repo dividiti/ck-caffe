c44ce17219229367e86412505372283a284b9414
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-07-09 16:31:56 +0100

    calculating and saving number of original images in lmdb dataset ...

--- soft/dataset.imagenet.val.lmdb/customize.py
+++ soft/dataset.imagenet.val.lmdb/customize.py
@@ -55,6 +55,8 @@ def setup(i):
     ck=i['ck_kernel']
     s=''
 
+    cdeps=i['deps_copy'] # even if original is removed, we still have a copy to figure out number of images
+
     iv=i.get('interactive','')
 
     cus=i.get('customize',{})
@@ -82,5 +84,33 @@ def setup(i):
     env[ep]=pi
 
     env['CK_CAFFE_IMAGENET_VAL_LMDB']=pi
+
+    # Checking number of images
+    pff=cus['ck_features_file']
+    pf=os.path.join(pi, pff)
+    features=cus.get('features',{})
+
+    pim=cdeps.get('dataset-imagenet-raw',{}).get('dict',{}).get('env',{}).get('CK_CAFFE_IMAGENET_VAL','')
+    if pim=='':
+       if os.path.isfile(pf):
+          r=ck.load_json_file({'json_file':pf})
+          if r['return']>0: return r
+          cus['features']=r['dict']
+       else:
+          return {'return':1, 'error':'CK features for DNN lmdb dataset are not defined and file '+pff+' doesn\'t exist'}
+
+    else:
+       dl=os.listdir(pim)
+
+       num=0
+
+       for fn in dl:
+           if fn.endswith('.JPEG') or fn.endswith('.jpeg'):
+              num+=1
+
+       features['number_of_original_images']=num
+
+       r=ck.save_json_to_file({'json_file':pf, 'dict':features})
+       if r['return']>0: return r
 
     return {'return':0, 'bat':s}
