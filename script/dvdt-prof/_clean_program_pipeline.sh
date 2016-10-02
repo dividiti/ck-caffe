8c972699f47415d4d8b71b95cf89d5f63d66184c
Anton Lokhmotov <anton@dividiti.com>
2016-10-02 17:08:02 +0000

    Record Caffe experiments with OpenCL profile.

---
+++ script/dvdt-prof/_clean_program_pipeline.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo {} > _setup_program_pipeline_tmp.json
---
+++ script/dvdt-prof/dvdt_prof_alexnet_clblas.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ck autotune pipeline:program pipeline_from_file=_setup_program_pipeline_tmp.json @dvdt_prof_alexnet_clblas.json "$@"
