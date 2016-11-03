8044f32ca7de2298293e7048ef35ffc5a9ff71e9
Anton Lokhmotov <anton@dividiti.com>
2016-11-03 09:27:38 +0000

    Update the clean and clobber scripts (to be obsoleted soon).

--- script/explore-batch-size-by-tags/_explore_batch_size_by_tags_clean.sh
+++ script/explore-batch-size-by-tags/_explore_batch_size_by_tags_clean.sh
@@ -4,4 +4,4 @@ rm -f pipeline.*.*.tmp.json
 rm -f pipeline.*.*.answers
 rm -f explore.*.*.json
 rm -f explore.*.*.sh
-rm -f explore_batch_size_by_tags.py
+rm -f explore_batch_size_by_tags-benchmark.py
--- script/explore-batch-size-by-tags/_explore_batch_size_by_tags_clobber.sh
+++ script/explore-batch-size-by-tags/_explore_batch_size_by_tags_clobber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./_explore_batch_size_by_tags_clean.sh
-ck rm experiment:* --tags=gpu_time
+ck rm experiment:* --tags=explore-batch-size-by-tags
