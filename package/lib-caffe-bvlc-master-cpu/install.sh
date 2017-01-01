b61f08639e1ed3e53ff8ea1886272de09566a827
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-01-01 18:21:00 +0000

    added deps on gflags, glogs and protobuf - it's now possible to compile with latest protobuf

--- package/lib-caffe-bvlc-master-cpu/install.sh
+++ package/lib-caffe-bvlc-master-cpu/install.sh
@@ -67,7 +67,13 @@ echo "Building Caffe in '${CAFFE_BLD_DIR}' ..."
 mkdir -p ${CAFFE_BLD_DIR}
 cd ${CAFFE_BLD_DIR}
 
-make -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS}
+# When using protobuf and many processors, build fails
+JOBS=${CK_HOST_CPU_NUMBER_OF_PROCESSORS}
+if [ "$JOBS" -gt "4" ] ; then
+  JOBS=4
+fi
+
+make -j $JOBS
 if [ "${?}" != "0" ] ; then
   echo "Error: Building Caffe in '${CAFFE_BLD_DIR}' failed!"
   exit 1
