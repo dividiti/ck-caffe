1ddd69fb7b40ca641772e13d43748e0383219954
defremov <defremov@ac-sw.com>
2017-04-09 01:03:24 +0700

    issue #54 caffe-time-opencl add skip forward backward

--- program/caffe-time-opencl/caffe.cpp
+++ program/caffe-time-opencl/caffe.cpp
@@ -394,6 +394,10 @@ int test() {
 // Time: benchmark the execution time of a model.
 int time() {
   CHECK_GT(FLAGS_model.size(), 0) << "Need a model definition to time.";
+
+  const bool skip_fw = getenv("CK_CAFFE_SKIP_FORWARD")  ? true : false;
+  const bool skip_bw = getenv("CK_CAFFE_SKIP_BACKWARD") ? true : false;
+
   caffe::Phase phase = get_phase_from_flags(caffe::TRAIN);
   vector<string> stages = get_stages_from_flags();
 
@@ -424,11 +428,17 @@ int time() {
   LOG(INFO) << "Performing Forward";
   // Note that for the speed benchmark, we will assume that the network does
   // not take any input blobs.
-  float initial_loss;
-  caffe_net.Forward(&initial_loss);
+  float initial_loss = 0.0f;
+  if (!skip_fw) {
+    LOG(INFO) << "Performing Forward";
+    caffe_net->Forward(&initial_loss);
+  }
+
   LOG(INFO) << "Initial loss: " << initial_loss;
-  LOG(INFO) << "Performing Backward";
-  caffe_net.Backward();
+  if (!skip_bw) {
+    LOG(INFO) << "Performing Backward";
+    caffe_net.Backward();
+  }
 
   const vector<shared_ptr<Layer<float> > >& layers = caffe_net.layers();
   const vector<vector<Blob<float>*> >& bottom_vecs = caffe_net.bottom_vecs();
@@ -449,23 +459,28 @@ int time() {
   for (int_tp j = 0; j < FLAGS_iterations; ++j) {
     Timer iter_timer;
     iter_timer.Start();
-    forward_timer.Start();
-    for (int_tp i = 0; i < layers.size(); ++i) {
-      timer.Start();
-      layers[i]->Forward(bottom_vecs[i], top_vecs[i]);
-      Caffe::Synchronize(Caffe::GetDefaultDevice()->id());
-      forward_time_per_layer[i] += timer.MicroSeconds();
+    if (!skip_fw) {
+      forward_timer.Start();
+      for (int_tp i = 0; i < layers.size(); ++i) {
+        timer.Start();
+        layers[i]->Forward(bottom_vecs[i], top_vecs[i]);
+        Caffe::Synchronize(Caffe::GetDefaultDevice()->id());
+        forward_time_per_layer[i] += timer.MicroSeconds();
+      }
+      forward_time += forward_timer.MicroSeconds();
     }
-    forward_time += forward_timer.MicroSeconds();
-    backward_timer.Start();
-    for (int_tp i = layers.size() - 1; i >= 0; --i) {
-      timer.Start();
-      layers[i]->Backward(top_vecs[i], bottom_need_backward[i],
-                          bottom_vecs[i]);
-      Caffe::Synchronize(Caffe::GetDefaultDevice()->id());
-      backward_time_per_layer[i] += timer.MicroSeconds();
+
+    if (!skip_bw) {
+      backward_timer.Start();
+      for (int_tp i = layers.size() - 1; i >= 0; --i) {
+        timer.Start();
+        layers[i]->Backward(top_vecs[i], bottom_need_backward[i],
+                            bottom_vecs[i]);
+        Caffe::Synchronize(Caffe::GetDefaultDevice()->id());
+        backward_time_per_layer[i] += timer.MicroSeconds();
+      }
+      backward_time += backward_timer.MicroSeconds();
     }
-    backward_time += backward_timer.MicroSeconds();
     LOG(INFO) << "Iteration: " << j + 1 << " forward-backward time: "
       << iter_timer.MilliSeconds() << " ms.";
   }
