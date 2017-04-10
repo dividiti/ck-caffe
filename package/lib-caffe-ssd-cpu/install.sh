f993473be8930cb49f0cdf7e4d84382b34e42f03
Unknown <anatolybelonog@gmail.com>
2017-04-10 17:26:12 +0700

    added caffe ssd cpu support

--- package/lib-caffe-ssd-cpu/install.sh
+++ package/lib-caffe-ssd-cpu/install.sh
@@ -1,32 +1,75 @@
 #! /bin/bash
 
-# CK installation script for TensorFlow package from Makefiles
-# http://cKnowledge.org/ai
+#
+# Installation script for Caffe.
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, 2015;
+# - Anton Lokhmotov, 2016.
+#
 
-CAFFE_SRC=${INSTALL_DIR}/src
-cd ${CAFFE_SRC}
+# PACKAGE_DIR
+# INSTALL_DIR
 
-if [ "${CK_ENV_COMPILER_LLVM_BIN}" != "" ] ; then
-  CK_FULL_CXX="${CK_ENV_COMPILER_LLVM_BIN}/${CK_CXX}"
-  CK_FULL_AR="${CK_ENV_COMPILER_LLVM_BIN}/${CK_AR}"
-  CK_FULL_LD="${CK_ENV_COMPILER_LLVM_BIN}/${CK_LD}"
-elif [ "${CK_ENV_COMPILER_GCC_BIN}" != "" ] ; then
-  CK_FULL_CXX="${CK_ENV_COMPILER_GCC_BIN}/${CK_CXX}"
-  CK_FULL_AR="${CK_ENV_COMPILER_GCC_BIN}/${CK_AR}"
-  CK_FULL_LD="${CK_ENV_COMPILER_GCC_BIN}/${CK_LD}"
-else
-  CK_FULL_CXX=${CK_CXX}
-  CK_FULL_AR=${CK_AR}
-  CK_FULL_LD=${CK_LD}
+echo "**************************************************************"
+echo "Preparing vars for Caffe ..."
+
+CK_OPENMP="-fopenmp"
+if [ "${CK_HAS_OPENMP}" = "0"  ]; then
+  CK_OPENMP=""
 fi
 
-CPU_ONLY=1
-BLAS_INCLUDE=${CK_ENV_LIB_OPENBLAS_INCLUDE}
-BLAS_LIB=${CK_ENV_LIB_OPENBLAS_LIB}
+# Check extra stuff
+EXTRA_FLAGS=""
 
-cp Makefile.config.example Makefile.config
+cd ${INSTALL_DIR}/obj
 
-make CUSTOM_CXX="${CK_FULL_CXX}" CPU_ONLY="${CPU_ONLY}" BLAS_INCLUDE="${BLAS_INCLUDE}" BLAS_LIB="${BLAS_LIB}" -j8
+cmake -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
+      -DCMAKE_C_COMPILER="${CK_CC_PATH_FOR_CMAKE}" \
+      -DCMAKE_C_FLAGS="${CK_CC_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS}" \
+      -DCMAKE_CXX_COMPILER="${CK_CXX_PATH_FOR_CMAKE}" \
+      -DCMAKE_CXX_FLAGS="${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -I${CK_ENV_LIB_OPENCV_INCLUDE}" \
+      -DCMAKE_AR="${CK_AR_PATH_FOR_CMAKE}" \
+      -DCMAKE_LINKER="${CK_LD_PATH_FOR_CMAKE}" \
+      -DCMAKE_SHARED_LINKER_FLAGS="$CK_OPENMP" \
+      -DBUILD_python=OFF \
+      -DBUILD_docs=OFF \
+      -DCPU_ONLY=ON \
+      -DUSE_OPENMP:BOOL=${USE_OPENMP} \
+      -DUSE_GREENTEA=OFF \
+      -DUSE_LMDB=ON \
+      -DUSE_LEVELDB=ON \
+      -DUSE_HDF5=ON \
+      -DBLAS=open \
+      -DGFLAGS_INCLUDE_DIR="${CK_ENV_LIB_GFLAGS_INCLUDE}" \
+      -DGLOG_INCLUDE_DIR="${CK_ENV_LIB_GLOG_INCLUDE}" \
+      -DBoost_ADDITIONAL_VERSIONS="1.62" \
+      -DBOOST_ROOT=${CK_ENV_LIB_BOOST} \
+      -DBOOST_INCLUDEDIR="${CK_ENV_LIB_BOOST_INCLUDE}" \
+      -DBOOST_LIBRARYDIR="${CK_ENV_LIB_BOOST_LIB}" \
+      -DBoost_INCLUDE_DIR="${CK_ENV_LIB_BOOST_INCLUDE}" \
+      -DBoost_LIBRARY_DIR="${CK_ENV_LIB_BOOST_LIB}" \
+      -DHDF5_DIR="${CK_ENV_LIB_HDF5}/cmake" \
+      -DHDF5_ROOT_DIR="${CK_ENV_LIB_HDF5}/cmake" \
+      -DHDF5_INCLUDE_DIRS="${CK_ENV_LIB_HDF5_INCLUDE}" \
+      -DOpenBLAS_INCLUDE_DIR="${CK_ENV_LIB_OPENBLAS_INCLUDE}" \
+      -DOpenBLAS_LIB="${CK_ENV_LIB_OPENBLAS_LIB}/libopenblas.a" \
+      -DLMDB_INCLUDE_DIR="${CK_ENV_LIB_LMDB_INCLUDE}" \
+      -DOpenCV_DIR="${CK_ENV_LIB_OPENCV_JNI}" \
+      -DPROTOBUF_INCLUDE_DIR="${CK_ENV_LIB_PROTOBUF_HOST_INCLUDE}" \
+      -DPROTOBUF_PROTOC_EXECUTABLE="${CK_ENV_LIB_PROTOBUF_HOST}/bin/protoc" \
+      -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}/install" \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      ../src
 
-exit 0
-#make py
+if [ "${?}" != "0" ] ; then
+  echo "Error: cmake failed!"
+  exit 1
+fi
+
+cp ${INSTALL_DIR}/src/include/caffe ${INSTALL_DIR}/obj/include
+
+return 0
