646ce92362b1c634ea827ccbece7a919a5af1159
Grigori Fursin <grigori.fursin@cTuning.org>
2017-04-26 10:00:26 +0200

    support for Python on Windows

--- package/lib-caffe-bvlc-master-cuda-universal/scripts.win/install.bat
+++ package/lib-caffe-bvlc-master-cuda-universal/scripts.win/install.bat
@@ -12,6 +12,16 @@ rem
 rem PACKAGE_DIR
 rem INSTALL_DIR
 
+if "%CAFFE_BUILD_PYTHON%" == "ON" (
+  echo.
+  echo You are compiling Caffe with Python support!
+  echo To use it you need to set up CK env as following ^(after installation^)^:
+  echo.
+  echo ck xset env tags=lib,caffe ; call tmp-ck-env.bat ; ipython2
+  echo.
+  set /p id="Press enter to continue"
+)
+
 echo **************************************************************
 echo Preparing vars for Caffe ...
 
@@ -24,7 +34,7 @@ set CK_CMAKE_EXTRA=%CK_CMAKE_EXTRA% -DCPU_ONLY:BOOL=%CPU_ONLY% ^
  -DUSE_LEVELDB=OFF ^
  -DUSE_PREBUILT_DEPENDENCIES=OFF ^
  -DBUILD_SHARED_LIBS:BOOL=%CMAKE_BUILD_SHARED_LIBS% ^
- -DBUILD_python:BOOL=%BUILD_PYTHON% ^
+ -DBUILD_python=%CAFFE_BUILD_PYTHON% ^
  -DBUILD_matlab:BOOL=%BUILD_MATLAB% ^
  -DGFLAGS_INCLUDE_DIR="%CK_ENV_LIB_GFLAGS_INCLUDE%" ^
  -DGFLAGS_LIBRARY="%CK_ENV_LIB_GFLAGS_LIB%\gflags.lib" ^
