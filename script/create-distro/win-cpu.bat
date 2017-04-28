ef8d29fd05f990cf598c1ccb544c1b00ae989001
Grigori Fursin <grigori.fursin@cTuning.org>
2017-04-28 12:40:42 +0200

    added support to create binary package distributions (useful for crowd-benchmarking and crowd-tuning particularly on Windows) ...

---
+++ script/create-distro/win-cpu.bat
@@ -0,0 +1,1 @@
+ck distribute package:lib-caffe-bvlc-master-cpu-universal --uid=80f6a04b578c68cf
---
+++ script/create-distro/win-opencl-libdnn-viennacl.bat
@@ -0,0 +1,1 @@
+ck distribute package:lib-caffe-bvlc-opencl-libdnn-viennacl-universal --uid=c3aad94c02926318
