78948390b6303f0daf9fa852191f2109c5409e58
Dmitry Savenko <dsavenko@xored.com>
2017-05-18 16:55:46 +0700

    Simplified and fixed SSD-CUDA package

--- package/lib-caffe-ssd-cuda/scripts.linux/install.sh
+++ package/lib-caffe-ssd-cuda/scripts.linux/install.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+#
+# Installation script for Caffe.
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, 2015;
+# - Anton Lokhmotov, 2016.
+#
+
 # PACKAGE_DIR
 # INSTALL_DIR
 
@@ -14,28 +25,36 @@ fi
 # Check extra stuff
 EXTRA_FLAGS=""
 
+XCMAKE_AR=""
+if [ "${CK_AR_PATH_FOR_CMAKE}" != "" ] ; then
+    XCMAKE_AR=" -DCMAKE_AR=${CK_AR_PATH_FOR_CMAKE} "
+fi
+
+XCMAKE_LD=""
+if [ "${CK_LD_PATH_FOR_CMAKE}" != "" ] ; then
+    XCMAKE_LD=" -DCMAKE_LINKER=${CK_LD_PATH_FOR_CMAKE} "
+fi
+
 cd ${INSTALL_DIR}/obj
 
 cmake -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
       -DCMAKE_C_COMPILER="${CK_CC_PATH_FOR_CMAKE}" \
-      -DCMAKE_C_FLAGS="${CK_CC_FLAGS_FOR_CMAKE} ${CK_CC_FLAGS_ANDROID_TYPICAL} ${EXTRA_FLAGS}" \
+      -DCMAKE_C_FLAGS="${CK_CC_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS}" \
       -DCMAKE_CXX_COMPILER="${CK_CXX_PATH_FOR_CMAKE}" \
       -DCMAKE_CXX_FLAGS="${CK_CXX_FLAGS_FOR_CMAKE} ${EXTRA_FLAGS} -I${CK_ENV_LIB_OPENCV_INCLUDE}" \
-      -DCMAKE_AR="${CK_AR_PATH_FOR_CMAKE}" \
-      -DCMAKE_LINKER="${CK_LD_PATH_FOR_CMAKE}" \
+      ${XCMAKE_AR} \
+      ${XCMAKE_LD} \
       -DCMAKE_SHARED_LINKER_FLAGS="$CK_OPENMP" \
-      -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF \
-      -DBUILD_python=OFF \
+      -DBUILD_python=ON \
+      -DPYTHON_EXECUTABLE:FILEPATH="$CK_ENV_COMPILER_PYTHON_FILE" \
       -DBUILD_docs=OFF \
-      -DCPU_ONLY=OFF \
-      -DUSE_OPENMP:BOOL=${USE_OPENMP} \
+      -DCPU_ONLY=$CPU_ONLY \
+      -DUSE_OPENMP=OFF \
       -DUSE_GREENTEA=OFF \
       -DUSE_LMDB=ON \
       -DUSE_LEVELDB=ON \
       -DUSE_HDF5=ON \
       -DBLAS=open \
-      -DCUDA_TOOLKIT_ROOT_DIR="${CK_ENV_COMPILER_CUDA}" \
-      -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES -Wno-deprecated-gpu-targets" \
       -DGFLAGS_INCLUDE_DIR="${CK_ENV_LIB_GFLAGS_INCLUDE}" \
       -DGLOG_INCLUDE_DIR="${CK_ENV_LIB_GLOG_INCLUDE}" \
       -DBoost_ADDITIONAL_VERSIONS="1.62" \
@@ -54,13 +73,13 @@ cmake -DCMAKE_BUILD_TYPE=${CK_ENV_CMAKE_BUILD_TYPE:-Release} \
       -DPROTOBUF_INCLUDE_DIR="${CK_ENV_LIB_PROTOBUF_HOST_INCLUDE}" \
       -DPROTOBUF_PROTOC_EXECUTABLE="${CK_ENV_LIB_PROTOBUF_HOST}/bin/protoc" \
       -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}/install" \
-      ../src
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      ${PACKAGE_CONFIGURE_FLAGS} \
+      ${INSTALL_DIR}/${PACKAGE_SUB_DIR}
 
 if [ "${?}" != "0" ] ; then
   echo "Error: cmake failed!"
   exit 1
 fi
-
-export CK_MAKE_EXTRA="VERBOSE=1"
 
 return 0
