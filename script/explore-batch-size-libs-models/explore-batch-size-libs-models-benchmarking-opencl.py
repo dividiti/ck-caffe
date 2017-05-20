91f9b1b4a67516acc4abc180bf31b639ac41a5c4
Flavio Vella <flavio@dividiti.com>
2017-05-20 11:40:51 +0100

    Pipeline lib by lib

--- script/explore-batch-size-libs-models/explore-batch-size-libs-models-benchmarking-opencl.py
+++ script/explore-batch-size-libs-models/explore-batch-size-libs-models-benchmarking-opencl.py
@@ -158,7 +158,9 @@ def do(i, arg):
         lib_tags=re.match('BVLC Caffe framework \((?P<tags>.*)\)', lib_name)
         lib_tags=lib_tags.group('tags').replace(' ', '').replace(',', '-')
         # Skip some libs with "in [..]" or "not in [..]".
-        if lib_tags not in ['opencl-clblast-tune', 'opencl-clblast']: continue
+        if lib_tags not in ['opencl-clblast-tune']: continue
+
+#        if lib_tags not in ['opencl-clblast-tune', 'opencl-clblast']: continue
 #        if lib_tags not in ['opencl-libdnn-clblast-tune', 'opencl-libdnn-clblast']: continue
 #        if lib_tags not in ['opencl-libdnn-viennacl', 'opencl-viennacl']: continue
         
