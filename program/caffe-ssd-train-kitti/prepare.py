0bb1d5b811ecf92207d9745a97d6f3f1cf12a3e7
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-01-31 19:23:49 +0700

    training script

--- program/caffe-ssd-train-kitti/prepare.py
+++ program/caffe-ssd-train-kitti/prepare.py
@@ -8,44 +8,55 @@
 #
 
 import os
-import shutil
-import subprocess
-import numpy as np  
+import numpy as np
+import utils
 
 SRC_IMAGES_DIR = os.getenv('CK_ENV_DATASET_IMAGE_DIR')
 SRC_LABELS_DIR = os.getenv('CK_ENV_DATASET_LABELS_DIR')
-TRAIN_PERCENT = int(os.getenv('CK_TRAIN_IMAGES_PERCENT', 70))
 CAFFE_BIN_DIR = os.getenv('CK_ENV_LIB_CAFFE_BIN')
+CAFFE_MODEL_DIR = os.getenv('CK_ENV_MODEL_CAFFE')
 
 CUR_DIR = os.path.realpath('.')
 TMP_LABELS_DIR = os.path.join(CUR_DIR, 'labels')
 TRAIN_FILE_LIST = os.path.join(CUR_DIR, 'train.txt')
-VAL_FILE_LIST = os.path.join(CUR_DIR, 'val.txt')
+TEST_FILE_LIST = os.path.join(CUR_DIR, 'test.txt')
+TRAIN_PERCENT = int(os.getenv('CK_TRAIN_IMAGES_PERCENT', 70))
 
-TARGET_IMG_W = 300 # TODO depend on SSD model: ssd-300 or ssd-500
-TARGET_IMG_H = 300 # TODO depend on SSD model: ssd-300 or ssd-500
-TARGET_TRAIN_LMDB = os.path.join(CUR_DIR, 'train_lmdb')
-TARGET_VAL_LMDB = os.path.join(CUR_DIR, 'val_lmdb')
+# Should be more robust criterion, may be lib should proivde some env var
+TARGET_IMG_W = 300 if '-300' in CAFFE_MODEL_DIR else 500
+TARGET_IMG_H = TARGET_IMG_W
 
-LABEL_MAP_FILE = os.path.join(os.path.realpath('..'), 'labelmap_kitti.prototxt')
+TRAIN_LMDB = os.path.join(CUR_DIR, 'train_lmdb')
+TEST_LMDB = os.path.join(CUR_DIR, 'test_lmdb')
+NAME_SIZE_FILE = os.path.join(CUR_DIR, 'test_name_size.txt')
 
-# These ids should be the same as in LABEL_MAP_FILE
-LABEL_MAP = {
-  'Car': 0,
-  'Cyclist': 1,
-  'Pedestrian': 2
-}
+LABEL_MAP = {}
+LABEL_MAP_FILE = os.path.join(CUR_DIR, 'labelmap_kitti.prototxt')
 
 
-def get_label_id(label):
+def save_label_to_map(label):
   '''
   While KITTI annotation files contain text name for label,
-  SSD treats label as integer id. We keep label ids as static data
-  here for simplisity and because of KITTI has only a few of them,
-  but they could be read from LABEL_MAP_FILE
+  SSD treats label as integer id. 
   '''
-  return LABEL_MAP[label] if label in LABEL_MAP else -1
+  if label in LABEL_MAP:
+    return LABEL_MAP[label]
+  label_id = len(LABEL_MAP)
+  LABEL_MAP[label] = label_id
+  return label_id
 
+
+def save_label_map_file():
+  with open(LABEL_MAP_FILE, 'w') as f:
+    for label in LABEL_MAP:
+      s = 'item {{\n' \
+          '  name: "{0}"\n' \
+          '  label: {1}\n' \
+          '  display_name: "{0}"\n' \
+          '}}\n'\
+          .format(label, LABEL_MAP[label])
+      f.write(s)
+    
 
 def convert_labels():
   '''
@@ -69,9 +80,7 @@ def convert_labels():
    13) 38.26   - ?
    14) 1.57    - ?
   '''
-  if os.path.isdir(TMP_LABELS_DIR):
-    shutil.rmtree(TMP_LABELS_DIR)
-  os.mkdir(TMP_LABELS_DIR)
+  utils.mkdir(TMP_LABELS_DIR)
   
   for label_file in os.listdir(SRC_LABELS_DIR):
     src_label_path = os.path.join(SRC_LABELS_DIR, label_file)
@@ -80,13 +89,19 @@ def convert_labels():
       with open(dst_label_path, 'w') as dst_file:
         for line in src_file:
           cols = line.split(' ')
-          label_id = get_label_id(cols[0])
+          label_id = save_label_to_map(cols[0])
           if label_id >= 0:
             dst_file.write('{} {} {} {} {}\n'.format(
               label_id, cols[4], cols[5], cols[6], cols[7]))
+
+  save_label_map_file()
     
 
 def write_file_list(img_files, file_name):
+  '''
+  Makes images list file. Each line of this list contains 
+  full path to an image and path to corresponding label file.
+  '''
   print('Writing {}...'.format(file_name))
   print('Images count: {}'.format(len(img_files)))
   with open(file_name, 'w') as f:
@@ -98,22 +113,34 @@ def write_file_list(img_files, file_name):
       f.write('{} {}\n'.format(img_path, label_path))
 
 
-def make_train_val_file_lists():
+def make_train_test_file_lists():
   '''
   Splits all images into two random sets -
-  one for train and another for validation.
+  one for train and another for test.
   '''
   all_images = os.listdir(SRC_IMAGES_DIR)
   all_images = np.random.permutation(all_images) 
   print('Total images count: {}'.format(len(all_images)))
   train_count = int(len(all_images) * TRAIN_PERCENT / 100.0)
   write_file_list(all_images[:train_count], TRAIN_FILE_LIST)
-  write_file_list(all_images[train_count:], VAL_FILE_LIST)
+  write_file_list(all_images[train_count:], TEST_FILE_LIST)
+
+  # Generate image name and size infomation.
+  cmd = []
+  cmd.append(os.path.join(CAFFE_BIN_DIR, 'get_image_size'))
+  cmd.append('') # we can leave root path empty as our file list contains absolute pathes
+  cmd.append(TEST_FILE_LIST)
+  cmd.append(NAME_SIZE_FILE)
+  utils.run_command(cmd)
 
 
 def make_lmdb(list_file, out_dir):
-  if os.path.isdir(out_dir):
-    shutil.rmtree(out_dir)
+  '''
+  Use convert_annoset tool from caffe SSD branch.
+  The tool takes a list file as parameter and writes
+  all listed images and its labels into LBDM database.
+  '''
+  utils.rmdir(out_dir)
     
   cmd = []
   cmd.append(os.path.join(CAFFE_BIN_DIR, 'convert_annoset'))
@@ -128,10 +155,7 @@ def make_lmdb(list_file, out_dir):
   cmd.append('') # we can leave root path empty as our file list contains absolute pathes
   cmd.append(list_file)
   cmd.append(out_dir)
-  print(' '.join(cmd))
-  process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-  output = process.communicate()[0]
-  print(output)
+  utils.run_command(cmd)
 
 
 if __name__ == '__main__':
@@ -139,10 +163,20 @@ if __name__ == '__main__':
   convert_labels()
 
   print('Making image list files...')
-  make_train_val_file_lists()
+  make_train_test_file_lists()
 
   print('Making training lmdb...')
-  make_lmdb(TRAIN_FILE_LIST, TARGET_TRAIN_LMDB)
+  make_lmdb(TRAIN_FILE_LIST, TRAIN_LMDB)
 
-  print('Making validation lmdb...')
-  make_lmdb(VAL_FILE_LIST, TARGET_VAL_LMDB)
+  print('Making testing lmdb...')
+  make_lmdb(TEST_FILE_LIST, TEST_LMDB)
+
+  # Save some info about prepared data to resue it in training script.
+  utils.write_json('info.json', {
+    'num_classes': len(LABEL_MAP),
+    'img_width': TARGET_IMG_W,
+    'img_height': TARGET_IMG_H
+  })
+
+  print('OK')
+  print('Use "train" command key to start training session')
--- program/caffe-ssd-train-kitti/train.py
+++ program/caffe-ssd-train-kitti/train.py
@@ -7,3 +7,114 @@
 # See CK LICENSE.txt for licensing details.
 #
 
+import os
+import re
+import utils
+
+CAFFE_BIN_DIR = os.getenv('CK_ENV_LIB_CAFFE_BIN')
+WEIGHTS_FILE = os.getenv('CK_ENV_MODEL_CAFFE_WEIGHTS_FILE')
+CAFFE_MODEL_DIR = os.getenv('CK_ENV_MODEL_CAFFE')
+SRC_SOLVER_PROTOTXT = os.path.join(CAFFE_MODEL_DIR, 'solver.prototxt')
+SRC_TRAIN_PROTOTXT = os.path.join(CAFFE_MODEL_DIR, 'train.prototxt')
+SRC_TEST_PROTOTXT = os.path.join(CAFFE_MODEL_DIR, 'test.prototxt')
+
+RUN_MODE_CPU = 'CPU'
+RUN_MODE_GPU = 'GPU'
+# Should be more robust criterion, may be lib should proivde some env var
+RUN_MODE = RUN_MODE_CPU if '-cpu' in CAFFE_BIN_DIR else RUN_MODE_GPU
+
+BATCH_SIZE = os.getenv('CK_BATCH_SIZE', 8)
+DEVICE_ID = os.getenv('CK_DEVICE_ID', 0)
+SNAPSHOTS_DIR = 'snapshots'
+SOLVER_PROTOTXT = 'solver.prototxt'
+TRAIN_PROTOTXT = 'train.prototxt'
+TEST_PROTOTXT = 'test.prototxt'
+TRAIN_LMDB = 'train_lmdb'
+TEST_LMDB = 'val_lmdb'
+LABEL_MAP_FILE = 'labelmap_kitti.prototxt'
+NAME_SIZE_FILE = 'test_name_size.txt'
+
+PREPARED_INFO = utils.read_json('info.json')
+NUM_CLASSES = PREPARED_INFO['num_classes']
+
+
+def set_param(txt, key, value, as_str = False):
+  key_marker = '^(\\s*{})\\:\\s+(.+)$'.format(key)
+  if as_str:
+    new_param = '\\1: "{}"'.format(value)
+  else:
+    new_param = '\\1: {}'.format(value)
+  return re.sub(key_marker, new_param, txt, 0, re.MULTILINE)
+
+
+def prepare_solver_file():
+  params = utils.read_text(SRC_SOLVER_PROTOTXT)
+  params = set_param(params, 'train_net', TRAIN_PROTOTXT, True)
+  params = set_param(params, 'test_net', TEST_PROTOTXT, True)
+  params = set_param(params, 'snapshot_prefix', SNAPSHOTS_DIR, True)
+  params = set_param(params, 'solver_mode', RUN_MODE)
+  params = set_param(params, 'device_id', DEVICE_ID)
+  # Another parameters could be taken from environment in the future
+  utils.write_text(SOLVER_PROTOTXT, params)
+
+
+def make_model_name(phase):
+  return 'VGG_kitti_SSD_{}x{}_{}'.format(
+    PREPARED_INFO['img_width'], PREPARED_INFO['img_height'], phase)
+
+
+def prepare_train_prototxt():
+  txt = utils.read_text(SRC_TRAIN_PROTOTXT)
+  #txt = set_param(txt, 'name', make_model_name('train'), True)
+  txt = set_param(txt, 'batch_size', BATCH_SIZE)
+  txt = set_param(txt, 'source', TRAIN_LMDB, True)
+  txt = set_param(txt, 'num_classes', NUM_CLASSES)
+  txt = set_param(txt, 'label_map_file', LABEL_MAP_FILE, True)
+  utils.write_text(TRAIN_PROTOTXT, txt)
+
+
+def prepare_test_prototxt():
+  txt = utils.read_text(SRC_TEST_PROTOTXT)
+  #txt = set_param(txt, 'name', make_model_name('test'), True)
+  txt = set_param(txt, 'batch_size', BATCH_SIZE)
+  txt = set_param(txt, 'source', TEST_LMDB)
+  txt = set_param(txt, 'num_classes', NUM_CLASSES)
+  txt = set_param(txt, 'label_map_file', LABEL_MAP_FILE, True)
+  txt = set_param(txt, 'name_size_file', NAME_SIZE_FILE, True)
+
+  reshape_layer = r'^(\s*reshape_param \{' \
+                      r'\s*shape \{' \
+                        r'\s*dim: 0' \
+                        r'\s*dim: -1' \
+                        r'\s*dim:) (\d+)$'
+  num_classes = r'\1 ' + str(NUM_CLASSES)
+  txt = re.sub(reshape_layer, num_classes, txt, 1, re.MULTILINE)
+  
+  utils.write_text(TEST_PROTOTXT, txt)
+    
+
+def start_training():
+  cmd = []
+  cmd.append(os.path.join(CAFFE_BIN_DIR, 'caffe'))
+  cmd.append('train')
+  cmd.append('--solver=' + SOLVER_PROTOTXT)
+  cmd.append('--weights=' + WEIGHTS_FILE)
+  if RUN_MODE == RUN_MODE_GPU:
+    cmd.append('--gpu=' + str(DEVICE_ID))
+  utils.run_command(cmd)
+
+
+if __name__ == '__main__':
+  utils.rmdir(SNAPSHOTS_DIR)
+
+  # Load default prototxt files from selected SSD caffemodel dir,
+  # modify them using new parameter values corresponding to current
+  # training session, and write as new files into current dir.
+  print('Preparing prototxts...')
+  prepare_train_prototxt()  
+  prepare_test_prototxt()  
+  prepare_solver_file()
+    
+  print('Training...')
+  start_training()
+      
