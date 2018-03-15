6313bd1f63c3e41e11eab7d9ea0d2618c4a2d333
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-03-15 17:24:16 +0000

    bugfix: Python 2.7 even on MacOSX still prefers .so extension

--- package/lib-caffe-bvlc-master-cpu-universal/scripts.linux/post-install2.sh
+++ package/lib-caffe-bvlc-master-cpu-universal/scripts.linux/post-install2.sh
@@ -8,6 +8,7 @@
 #
 # Developer(s):
 # - Grigori Fursin, 2017
+# - Leo Gordon, 2018
 #
 
 # PACKAGE_DIR
@@ -23,5 +24,13 @@ if [ ! -d "${CAFFE_LIB1}" ] && [ -d "${CAFFE_LIB2}" ] ; then
 
   mv ${CAFFE_LIB2} ${CAFFE_LIB1}
 fi
+
+    # on a Mac and compiled with Python support:
+if [ "$CK_DLL_EXT" = ".dylib" ] && [ "${CAFFE_BUILD_PYTHON}" == "ON" ]
+then
+    cd "${INSTALL_DIR}/install/python/caffe"
+    ln -s _caffe.dylib _caffe.so
+fi
+
 
 return 0
